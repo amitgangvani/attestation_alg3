b9d73225e23be353bbe8ad91ae73c16d5623a6fa
amitgangvani <amitgangvani@gmail.com>
2022-11-14 19:02:46 +0400

    Initial

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 class Message {
 
     author;
@@ -12,7 +11,12 @@ class Message {
     }
 
     show(){
-        console.log(`${this.time} ${this.author}: ${this.body}`);
+        let historyElem = document.getElementById('history');
+        historyElem.innerHTML = historyElem.innerHTML + this.toHtml();
+    }
+
+    toHtml(){
+        return `<p>${this.time} ${this.author}: ${this.body}</p>`;
     }
 
 }
@@ -26,6 +30,8 @@ class Messenger{
     }
 
     show_history(){
+        let historyElem = document.getElementById('history');
+        historyElem.innerHTML = '';
         this.messages.forEach(message => {
             message.show();
         });
@@ -38,8 +44,27 @@ function gettime() {
     return `${now.getHours()}:${now.getMinutes()}`
 }
 
+function sendMessage(){
+
+    let authorElem = document.getElementById('name');
+    let textElem = document.getElementById('message');
+
+    let author = authorElem.value;
+    let text = textElem.value;
+
+    if(!author || !text){
+       alert("Не указан автор или текст сообщения.");
+       return;
+    }
+
+    messenger.send(author, text);
+
+    authorElem.value = '';
+    textElem.value = '';
+}
+
+function showHistory(){
+    messenger.show_history();
+}
 
 let messenger = new Messenger();
-messenger.send('Артем', 'Первое сообщение');
-messenger.send('Мария', 'Второе сообщение');
-messenger.show_history();
